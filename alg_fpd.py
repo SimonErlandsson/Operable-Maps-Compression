2a28b501a12b73a1a7f98805e7397a3d5f81107d
Simon Erlandsson <simonsigge@hotmail.com>
2023-03-08 14:30:10 +0100

    Merge resolve

--- alg_fpd.py
+++ alg_fpd.py
@@ -13,6 +13,7 @@ from shapely import GeometryType as GT
 class Fpd(CompressionAlgorithm):
     MAX_CHUNK_SIZE = 10
     OPTIMAL_DELTA_SIZE = 4  # DEFAULT VALUEß
+    offset = 0 # Used when parsing
 
 # ---- HELPER METHODS
     def binary(self, num):
@@ -48,10 +49,9 @@ class Fpd(CompressionAlgorithm):
 
         elif geometry.geom_type == "MultiPolygon":
             for polygon in list(geometry.geoms):
-                subpoly_coord_count = 1 + len(polygon.interiors)
-                # subpoly_coord_count.append(len(list(polygon.exterior.coords)))
-                # for i in range(len(polygon.interiors)):
-                #     subpoly_coord_count.append(len(polygon.interiors[i].coords))
+                subpoly_coord_count.append(len(list(polygon.exterior.coords)))
+                for i in range(len(polygon.interiors)):
+                    subpoly_coord_count.append(len(polygon.interiors[i].coords))
         return subpoly_coord_count
     
 
@@ -93,7 +93,7 @@ class Fpd(CompressionAlgorithm):
     def decode_header(self, bin):
         delta_size, type = struct.unpack_from('!IB', bin)
         type = GT(type)
-        return delta_size, type
+        return delta_size, type, 5 # Offset is 5 bytes for IB
 
     def polygon_reset(self, res, chk_xs, chk_ys, chk_size_idx, geom_coord_count, chk_point_nbr):
         #Reset and store previous chk data if interrupted in the middle of a chk
@@ -103,7 +103,6 @@ class Fpd(CompressionAlgorithm):
             self.save_clear_state(res, chk_xs, chk_ys)
 
         subpoly_point_cnt = geom_coord_count.popleft()
-        print(subpoly_point_cnt)
         res.append(self.int_to_bytes(subpoly_point_cnt))
         return chk_point_nbr, subpoly_point_cnt
 
@@ -173,15 +172,48 @@ class Fpd(CompressionAlgorithm):
         if chk_point_nbr > 0:
             self.save_clear_state(res, chk_xs, chk_ys)
             res[chk_size_idx] = self.int_to_bytes(chk_point_nbr)
-        print([int.from_bytes(byte_val, "big") for byte_val in res])
         res = b''.join(res)
         return res, len(res)
 
+
+    def bytes_to_float32(self, bin):
+        val = struct.unpack_from('!f', bin, self.offset)[0] # Nbr of deltas
+        self.offset += 4
+        return val
+    
+    def bytes_to_float(self, bin, size=32):
+        # For now only bytes:
+        size //= 8
+        bin = bin[self.offset : self.offset + size]
+        print(bin)
+        self.offset += size
+        return bin
+
     def fp_delta_decoding(self, bin):
         geometry = shapely.LineString()
-        delta_size, type = self.decode_header(bin)
+        self.offset = 0
+        delta_size, type, offset = self.decode_header(bin)
         
         if type == GT.LINESTRING:
+            while (self.offset < len(bin)): # While != EOF
+                chk_size = struct.unpack_from('!I', bin, offset)[0] 
+                self.offset += 4
+                coords = []
+
+                # Extract reset point
+                x = self.bytes_to_float32(bin)
+                y = self.bytes_to_float32(bin)
+                coords.append((x, y))
+
+                # Loop through deltas in chunk
+                for i in range(chk_size):
+                    d_x = self.bytes_to_float(bin, size=delta_size * 8)
+                    d_y = self.bytes_to_float(bin, size=delta_size * 8)
+                    x += d_x # Add delta to old x value
+                    y += d_y
+                    coords.append((x, y))
+                #print(coords)
+
             pass
         elif type == GT.POLYGON:
             pass
@@ -327,9 +359,11 @@ class Fpd(CompressionAlgorithm):
 def main():
     x = Fpd()
     geom1 = shapely.wkt.loads("MULTIPOLYGON (((13.193709 55.7021381, 13.1937743 55.7021279, 13.1938355 55.7021184, 13.1938461 55.702109, 13.1938566 55.7020984, 13.1938611 55.7020902, 13.1938655 55.7020774, 13.1938655 55.7020633, 13.1938583 55.7020408, 13.1938402 55.7020014, 13.1937184 55.7017259, 13.1937008 55.7016876, 13.1936836 55.7016654, 13.1936537 55.7016428, 13.1936223 55.7016242, 13.1935741 55.7016036, 13.1935354 55.7015911, 13.1935006 55.701584, 13.1934829 55.701598, 13.1934673 55.7016115, 13.1934736 55.7016164, 13.1934776 55.7016216, 13.1934875 55.7016633, 13.1934985 55.7016898, 13.1935196 55.7017337, 13.1935659 55.7018353, 13.1936162 55.7018282, 13.1936551 55.7019155, 13.1936651 55.7019377, 13.1936955 55.7020047, 13.1936497 55.7020119, 13.193709 55.7021381)), ((13.1938175 55.7017126, 13.1938602 55.7017068, 13.1939048 55.7017007, 13.1938998 55.7016861, 13.193892 55.7016685, 13.1938831 55.7016589, 13.193871 55.701651, 13.1938602 55.701646, 13.1938405 55.7016438, 13.193822 55.7016456, 13.1938062 55.7016517, 13.1937985 55.7016571, 13.1937953 55.7016646, 13.1937979 55.7016746, 13.1938017 55.7016836, 13.1938052 55.7016908, 13.1938175 55.7017126)), ((13.1940245 55.7019788, 13.19398 55.7019848, 13.1939372 55.7019907, 13.1939585 55.7020383, 13.1939692 55.7020479, 13.1939841 55.7020512, 13.1939975 55.7020519, 13.1940079 55.702051, 13.1940198 55.7020497, 13.1940317 55.7020463, 13.1940395 55.7020422, 13.1940435 55.7020369, 13.1940452 55.7020314, 13.1940457 55.7020218, 13.1940245 55.7019788)), ((13.1939779 55.7015541, 13.1939529 55.701555, 13.1939622 55.7015658, 13.1939755 55.7015942, 13.194075 55.7018201, 13.1941382 55.7019637, 13.1941483 55.7019866, 13.194164 55.7020087, 13.1941899 55.7020304, 13.1942142 55.7020424, 13.1942291 55.7020486, 13.1942638 55.702042, 13.195019 55.7018988, 13.1948681 55.7018923, 13.1944181 55.7018687, 13.1944172 55.7018717, 13.194395 55.7018706, 13.1942164 55.7018622, 13.194172 55.7017564, 13.1941218 55.701761, 13.1941279 55.7017262, 13.1941357 55.7016818, 13.1940872 55.7015737, 13.1940769 55.7015503, 13.1939779 55.7015541), (13.1942341 55.7020059, 13.1942075 55.7020095, 13.1941895 55.7019673, 13.1941696 55.701921, 13.1941936 55.7019177, 13.1941884 55.7019055, 13.19426 55.7018958, 13.1942645 55.7019063, 13.1943172 55.7018991, 13.1943567 55.7019912, 13.1943041 55.7019984, 13.1943086 55.7020089, 13.1942394 55.7020183, 13.1942341 55.7020059)))")
-    t, bin3 = x.compress(geom1)
-    decomp = x.decompress(bin3)
-
+    geom2 = shapely.wkt.loads("LINESTRING (13.199378 55.7034667, 13.1999441 55.7033986, 13.200125 55.7033882, 13.2002723 55.7033936, 13.2004383 55.7034097, 13.2005935 55.7034211, 13.2007699 55.703423, 13.2011275 55.7034136, 13.2012413 55.7034103, 13.2012947 55.7034088)")
+    #t, bin3 = x.compress(geom2)
+    #print(bin3)
+    #decomp = x.decompress(bin3)
+    print(bytes(b'\xcf\x84\xcf\x81\xce\xbd\xcf\x82').binary())
     #print(x.calculate_delta_size(geom1))
     #t, bin = x.decompress(bin)
 
