9655a2bc48b1b6d61c387af83d14c27185162e8f
Simon Erlandsson <simonsigge@hotmail.com>
2023-03-09 12:41:41 +0100

    More encoding

--- alg_fpd.py
+++ alg_fpd.py
@@ -95,14 +95,17 @@ class Fpd(CompressionAlgorithm):
         self.offset += 5
         return delta_size, type # Offset is 5 bytes for IB
 
-    def polygon_reset(self, res, geom_coord_count, chk_point_nbr):
+    def polygon_reset(self, res, geom_coord_count, chk_point_cnt, chk_size_idx):
         #Reset and store previous chk data if interrupted in the middle of a chk
+        if chk_size_idx != 0:
+            res[chk_size_idx] = self.uint_to_bytes(chk_point_cnt - 1)
         subpoly_point_cnt = geom_coord_count.popleft()
         res.append(self.uint_to_bytes(subpoly_point_cnt))
-        return chk_point_nbr, subpoly_point_cnt
+        chk_point_cnt = 0
+        return chk_point_cnt, subpoly_point_cnt
 
         
-    def fp_delta_encoding(self, geometry, delta_size, max_chk_size):
+    def fp_delta_encoding(self, geometry, delta_size):
         coords = shapely.get_coordinates(geometry)
 
         #Saves interior and exterior lengths if polygon type
@@ -125,10 +128,10 @@ class Fpd(CompressionAlgorithm):
         #Loop all coordinates
         for i in range(len(coords)):
             if is_multipolygon and poly_ring_cnt == 0:
-               chk_point_cnt, poly_ring_cnt = self.polygon_reset(res, poly_coord_count, chk_point_cnt)
+               chk_point_cnt, poly_ring_cnt = self.polygon_reset(res, poly_coord_count, chk_point_cnt, chk_size_idx)
 
             if is_polygon and subpoly_point_cnt == 0:
-               chk_point_cnt, subpoly_point_cnt = self.polygon_reset(res, subpoly_coord_count, chk_point_cnt)
+               chk_point_cnt, subpoly_point_cnt = self.polygon_reset(res, subpoly_coord_count, chk_point_cnt, chk_size_idx)
 
             #----CHUNK HEADER INFORMATION (CHUNK SIZE, FULL FIRST COORDINATES)
             if chk_point_cnt == 0:
@@ -149,7 +152,7 @@ class Fpd(CompressionAlgorithm):
                     chk_size_idx = self.init_chunk(res, self.MAX_CHUNK_SIZE, coords, i)
                     chk_point_cnt = 1
 
-                if chk_point_cnt == max_chk_size:
+                if chk_point_cnt == self.MAX_CHUNK_SIZE:
                     chk_point_cnt = 0
 
             subpoly_point_cnt -= 1
@@ -274,7 +277,7 @@ class Fpd(CompressionAlgorithm):
         optimal_size = math.ceil(optimal_size / 8) * 8
         
         res, _ = self.fp_delta_encoding(
-            geometry, optimal_size, self.MAX_CHUNK_SIZE)
+            geometry, optimal_size)
         t = time.perf_counter()
         return t - s, res
 
@@ -341,12 +344,13 @@ def main():
     geom1 = shapely.wkt.loads("MULTIPOLYGON (((13.193709 55.7021381, 13.1937743 55.7021279, 13.1938355 55.7021184, 13.1938461 55.702109, 13.1938566 55.7020984, 13.1938611 55.7020902, 13.1938655 55.7020774, 13.1938655 55.7020633, 13.1938583 55.7020408, 13.1938402 55.7020014, 13.1937184 55.7017259, 13.1937008 55.7016876, 13.1936836 55.7016654, 13.1936537 55.7016428, 13.1936223 55.7016242, 13.1935741 55.7016036, 13.1935354 55.7015911, 13.1935006 55.701584, 13.1934829 55.701598, 13.1934673 55.7016115, 13.1934736 55.7016164, 13.1934776 55.7016216, 13.1934875 55.7016633, 13.1934985 55.7016898, 13.1935196 55.7017337, 13.1935659 55.7018353, 13.1936162 55.7018282, 13.1936551 55.7019155, 13.1936651 55.7019377, 13.1936955 55.7020047, 13.1936497 55.7020119, 13.193709 55.7021381)), ((13.1938175 55.7017126, 13.1938602 55.7017068, 13.1939048 55.7017007, 13.1938998 55.7016861, 13.193892 55.7016685, 13.1938831 55.7016589, 13.193871 55.701651, 13.1938602 55.701646, 13.1938405 55.7016438, 13.193822 55.7016456, 13.1938062 55.7016517, 13.1937985 55.7016571, 13.1937953 55.7016646, 13.1937979 55.7016746, 13.1938017 55.7016836, 13.1938052 55.7016908, 13.1938175 55.7017126)), ((13.1940245 55.7019788, 13.19398 55.7019848, 13.1939372 55.7019907, 13.1939585 55.7020383, 13.1939692 55.7020479, 13.1939841 55.7020512, 13.1939975 55.7020519, 13.1940079 55.702051, 13.1940198 55.7020497, 13.1940317 55.7020463, 13.1940395 55.7020422, 13.1940435 55.7020369, 13.1940452 55.7020314, 13.1940457 55.7020218, 13.1940245 55.7019788)), ((13.1939779 55.7015541, 13.1939529 55.701555, 13.1939622 55.7015658, 13.1939755 55.7015942, 13.194075 55.7018201, 13.1941382 55.7019637, 13.1941483 55.7019866, 13.194164 55.7020087, 13.1941899 55.7020304, 13.1942142 55.7020424, 13.1942291 55.7020486, 13.1942638 55.702042, 13.195019 55.7018988, 13.1948681 55.7018923, 13.1944181 55.7018687, 13.1944172 55.7018717, 13.194395 55.7018706, 13.1942164 55.7018622, 13.194172 55.7017564, 13.1941218 55.701761, 13.1941279 55.7017262, 13.1941357 55.7016818, 13.1940872 55.7015737, 13.1940769 55.7015503, 13.1939779 55.7015541), (13.1942341 55.7020059, 13.1942075 55.7020095, 13.1941895 55.7019673, 13.1941696 55.701921, 13.1941936 55.7019177, 13.1941884 55.7019055, 13.19426 55.7018958, 13.1942645 55.7019063, 13.1943172 55.7018991, 13.1943567 55.7019912, 13.1943041 55.7019984, 13.1943086 55.7020089, 13.1942394 55.7020183, 13.1942341 55.7020059)))")
     geom2 = shapely.wkt.loads("LINESTRING (13.199378 55.7034667, 13.1999441 55.7033986, 13.200125 55.7033882, 13.2002723 55.7033936, 13.2004383 55.7034097, 13.2005935 55.7034211, 13.2007699 55.703423, 13.2011275 55.7034136, 13.2012413 55.7034103, 13.2012947 55.7034088)")
     geom3 = shapely.wkt.loads('POLYGON ((13.1848537 55.7057363, 13.1848861 55.705646, 13.1848603 55.7056619, 13.1846238 55.7056422, 13.1846085 55.7057159, 13.1846356 55.7057179, 13.1848537 55.7057363), (13.1846694 55.705714, 13.1846543 55.7057128, 13.1846563 55.705705, 13.1846714 55.7057062, 13.1846694 55.705714), (13.1847425 55.7057123, 13.1847405 55.7057201, 13.1847254 55.7057188, 13.1847274 55.705711, 13.1847425 55.7057123), (13.1848001 55.7057179, 13.1848152 55.7057192, 13.1848131 55.705727, 13.1847981 55.7057258, 13.1848001 55.7057179), (13.1848068 55.7056929, 13.1848088 55.7056851, 13.1848239 55.7056863, 13.1848218 55.7056941, 13.1848068 55.7056929), (13.1847507 55.7056878, 13.1847356 55.7056865, 13.1847377 55.7056787, 13.1847528 55.70568, 13.1847507 55.7056878), (13.1846811 55.7056732, 13.184679 55.705681, 13.184664 55.7056798, 13.184666 55.705672, 13.1846811 55.7056732))')
-    geom4 = shapely.wkt.loads('POLYGON ((13.1848537 55.7057363, 13.1848861 55.705646, 13.1848537 55.7057363))')
+    geom4 = shapely.wkt.loads('POLYGON ((13.1848537 55.7057363, 13.1848861 55.705646, 13.1848537 55.7057363), (13.1847425 55.7057123, 13.1847405 55.7057201, 13.1847254 55.7057188, 13.1847274 55.705711, 13.1847425 55.7057123))')
     t, bin3 = x.compress(geom4)
     #print(x.bytes_to_float32(b'0xa70x300x530x41'))
     print(bin3.hex(sep=' '))
     #print("-DELTASIZE-_TY_POINTSINCHK_XFIRST-----------------_YFIRST-----------------")
     print("-DELTASIZE-_TY_RINGS------_POINTSINCHK_XFIRST-----------------_YFIRST-----------------_--XD1---------_--YD1---------_--XD2---------_--YD2---------_--XD3---------_--YD3---------_--XD4---------_--YD4---------_--XD5---------_--YD5---------_--XD6---------_--YD6---------_--XD7---------_--YD7---------_--XD8---------_--YD8---------_--XD9---------_--YD9---------")
+    print(shapely.to_ragged_array([geom4]))
     t, decomp = x.decompress(bin3)
     #print(x.bytes_to_float(bin3, size=16))
     #print(x.calculate_delta_size(geom1))
