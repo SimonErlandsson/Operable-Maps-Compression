08fecb4dcea6bafa2531d1413a4c4544d67e346e
Leo Westerberg <le8037we-s@student.lu.se>
2023-05-24 14:02:04 +0200

    prel final version of intersection and is_intersection

--- algos/fpd_extended_lib/cfg.py
+++ algos/fpd_extended_lib/cfg.py
@@ -5,7 +5,7 @@ CM = Enum('CM', ['ZLIB', 'GZIP', 'ARITHMETIC', 'NONE'])
 required_bits = lambda x: int(np.ceil(np.log2(x + 2)))
 
 # If true: use "normal" FPD, else use "32-bit integer reprs.".
-USE_DEFAULT_DOUBLE = False
+USE_DEFAULT_DOUBLE = True
 
 # Enable per delta entropy compression
 ENTROPY_METHOD = EM.NONE # 'HUFFMAN', 'GOLOMB', 'AUTO', 'NONE'.
@@ -16,11 +16,13 @@ CHUNK_COMP_METHOD = CM.NONE # 'ZLIB', 'GZIP', 'NONE'
 D_BITSIZE_SIZE = 12 # Size used to store the size of the chunks OPTIMIZE
 POLY_RING_CNT_SIZE = required_bits(2048)
 RING_CHK_CNT_SIZE = 30
-MAX_NUM_DELTAS = 15  # Max number of deltas in a chunk before split
+MAX_NUM_DELTAS = 17  # Max number of deltas in a chunk before split
 D_CNT_SIZE = required_bits(MAX_NUM_DELTAS + 1)
 
 BASELINE_ON = False
 # Disable optimized FPDE operations
+DISABLE_OPTIMIZED_UNPACKING = False
+
 DISABLE_OPTIMIZED_INTERSECTION = False if not BASELINE_ON else True
 DISABLE_OPTIMIZED_ADD_VERTEX = False if not BASELINE_ON else True
 DISABLE_OPTIMIZED_BOUNDING_BOX = False if not BASELINE_ON else True
